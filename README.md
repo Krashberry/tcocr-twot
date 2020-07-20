89fb7c75a530c1aeb71687f7eba95b6bc982b200
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2020-07-20 01:43:25 -0700

    Draft 1: MVP with the M doing a lot of work. Very little CSS since I had to start over last minute.

--- README.md
+++ README.md
@@ -9,20 +9,9 @@
 ![Wireframe](./project-1-wireframe.png)
 
 ## User Story
-    - The user clicks the 'Begin' button
-    - They are given some flavor text to set the mood, some background on the setting, and are introduced to the protagonist
-        + Button is just leading to next screen
-    - They are given the chance to make some observations as they make their way through town to the Vessel [a temple] of 'The Lady of The Water', should they choose to do so they may see some future NPCs
-        + Buttons let them observe some shops 
-    - They will then arrive at The Vessel where they will meet the local Eldist Sister who will inform them that something in the town's Od has seemed off but because they are not skilled with The Art they could not confirm if they were right about their misgivings or do anything about it and so they tell Candace [who I will refer to as The Player henceforth] about the Ward Points that they need to re-seal
-        + Buttons will allow The Player to ask the Eldist Sister some questions, which may lead to them getting an item   
-    - This will open things up for the player to choose which Ward they want to visit 
-        + Buttons will give the player options for which Ward to visit
-    - Each Ward will offer a small puzzle that the player will need to complete in order to re-empower it
-        + Buttons would be the options for going through the process of re-empowering the Ward. This could look like a series of riddles, which you may need to get a certain amount correct for to re-empower The Ward successfully, for example.
-    - Each Ward will take a day to re-empower overall.
-        + Buttons will allow The Player the option to choose from some tasks that they will have time to do one of the offered tasks before sleeping
-    - Once The Player has completed the re-empowerment of the outer wards they will need to re-empower the one at the Vessel. This will release a powerful Shard [a chaotic creature].
-        + Buttons will allow The Player some options for trying to re-seal the Shard or attack it. Based on a combination of the tasks completed and items collected The Player will finish the re-empowering of The Wards of Tideport and either re-seal the Shard or they will accidentally release the Shard into the world. 
-    - The game will then allow The Player some options about how they want to wrap things in Tideport up before they recieve a missive with their next assignment. 
-        + Buttons will allow The Player the option of sending their own letters, possibly recruiting an understudy [unsure of the difficulty of this], etc.
+- The user clicks one of the buttons on the start screen
+- They are given some flavor text to set the mood, some background on the setting, and are introduced to the protagonist
+- The player will travel throught the town and the market before finally getting to the Wards
+- The players score will be added to as they answer the riddles throughout the various levels and are exposed to more description of the Wards
+- After all of this the player will confront the creature in the Vessel of Tideport and then be taken to a screen showing their score in relation to how well they were able to answer the riddles and re-seal the Wards
+- The game will then reset itself after giving the player some time to see their score and read the closing text
--- js/app.js
+++ js/app.js
@@ -1,97 +1,133 @@
 import game from "./game.js";
+// where we are starting in the game
+let index = 0
+// control point where we handle what's shown in the contentBox
+const contentBox = document.querySelector('.contentBox')
+// control point where we handle the title of each scene
+let title = document.querySelector('.title')
+// control point where we handle the description of each scene
+const gameContent = document.querySelector('.description')
+// control point where we handle the gameText for each of the scenes
+const gameText = document.querySelector('.gameText')
+// control point where we handle the score for the answers in the game
+const score = document.querySelector('.score')
+// control points where we will handle the choices in each of the scenes
+let choice1 = document.querySelector('.choice1')
+let choice2 = document.querySelector('.choice2')
+let choice3 = document.querySelector('.choice3')
+let choice4 = document.querySelector('.choice4')
+// control point for where we will be starting the game when we load the game
+window.onload = function () {  
+  gameContent.innerText = game[game.currentLvl].content
+  gameText.innerHTML = game[game.currentLvl].query
+  title.innerHTML = game[game.currentLvl].title
+  choice1.value = "Next"
+  choice2.value = "Page"
+  choice3.value = "to"
+  choice4.value = "Start"
+}
 
-// index is acting as a counter for where we are in the scene array
-// let index = 0
+choice1.addEventListener('click', () => {
+  // moving to the next page
+  if (index < 7) { index++
+  // moving the game position forward
+    if (game[game.currentLvl].options[0].answer === true) {game.score++}
+  game.currentLvl = game.position[index]
+  // displaying the backdrop image for the current scene
+  contentBox.style.backgroundImage = game[game.currentLvl].backdrop
+  // grabbing the title and feeding it into the structure of the html
+  title.innerHTML = game[game.currentLvl].title
+  // grabbing the content and feeding it into the structure of the html
+  gameContent.innerText = game[game.currentLvl].content
+  // grabbing the query and feeding it into the structure of the html
+  gameText.innerHTML = game[game.currentLvl].query
+  // assigning each of the choice buttons to one of the actions [aka the game choices or riddle answers] available in the scene
+  choice1.value = game[game.currentLvl].options[0].action
+  choice2.value = game[game.currentLvl].options[1].action
+  choice3.value = game[game.currentLvl].options[2].action
+  choice4.value = game[game.currentLvl].options[3].action
+  // telling the game to not show any options if it's already got to the end of the game index
+    if (index === 7) {
+      choice2.style.display = "none" 
+      choice3.style.display = "none" 
+      choice4.style.display = "none" 
+    }
+  // telling it to end the game rather than go any further in index
+  } else {
+    endGame()
+  }
+})
 
-//  the contentBox is where we will feed in the things that will go into the scene so first we have to define it
-// let contentBox = document.querySelector('.contentBox')
+choice2.addEventListener('click', () => {
+  if (index < 7) { index++
+    if (game[game.currentLvl].options[1].answer === true) {game.score++}
+// moving to the next page
+  game.currentLvl = game.position[index]
+// moving the game position forward
+  contentBox.style.backgroundImage = game[game.currentLvl].backdrop
+  title.innerHTML = game[game.currentLvl].title
+  gameContent.innerText = game[game.currentLvl].content
+  gameText.innerHTML = game[game.currentLvl].query
+  choice1.value = game[game.currentLvl].options[0].action
+  choice2.value = game[game.currentLvl].options[1].action
+  choice3.value = game[game.currentLvl].options[2].action
+  choice4.value = game[game.currentLvl].options[3].action
+  if (index === 7) {
+    choice2.style.display = "none" 
+    choice3.style.display = "none" 
+    choice4.style.display = "none" 
+  }
+}
+})
 
-// // we let the program know to add a header to the CSS for the title of the scene, if it has one
-// let title = document.createElement('h2')
+choice3.addEventListener('click', () => {
+  if (index < 7) { index++
+    if (game[game.currentLvl].options[2].answer === true) {game.score++} 
+// moving to the next page
+  game.currentLvl = game.position[index]
+// moving the game position forward
+  contentBox.style.backgroundImage = game[game.currentLvl].backdrop
+  title.innerHTML = game[game.currentLvl].title
+  gameContent.innerText = game[game.currentLvl].content
+  gameText.innerHTML = game[game.currentLvl].query
+  choice1.value = game[game.currentLvl].options[0].action
+  choice2.value = game[game.currentLvl].options[1].action
+  choice3.value = game[game.currentLvl].options[2].action
+  choice4.value = game[game.currentLvl].options[3].action
+  if (index === 7) {
+    choice2.style.display = "none" 
+    choice3.style.display = "none" 
+    choice4.style.display = "none" 
+  }
+  }
+})
 
-// // we let the program know to add a paragrapgh to the CSS for the game text for the story, if it has any
-// let gameContent = document.createElement('p')
+choice4.addEventListener('click', () => {
+  if (index < 7) { index++
+    if (game[game.currentLvl].options[3].answer === true) {game.score++}
+// moving to the next page
+  game.currentLvl = game.position[index]
+// moving the game position forward
+  contentBox.style.backgroundImage = game[game.currentLvl].backdrop
+  title.innerHTML = game[game.currentLvl].title
+  gameContent.innerText = game[game.currentLvl].content
+  gameText.innerHTML = game[game.currentLvl].query
+  choice1.value = game[game.currentLvl].options[0].action
+  choice2.value = game[game.currentLvl].options[1].action
+  choice3.value = game[game.currentLvl].options[2].action
+  choice4.value = game[game.currentLvl].options[3].action
+  if (index === 7) {
+    choice2.style.display = "none" 
+    choice3.style.display = "none" 
+    choice4.style.display = "none" 
+  }
+  }
+})
 
-// // we let the program know we are going to have opitons for some of the scenes
-// let optionBox = document.querySelector('.optionBox')
-
-// // here we want to define the next button and go one up in the index of the game objects array of scenes when it is clicked on
-// let nextButton = document.querySelector('.next')
-
-// this has the program listen for the user clicking
-// nextButton.addEventListener('click', () => {
-//   // if selecterd item is the dagger we want the value to be true
-//   if (game.selectedItem === 'dagger') {
-//     game.haveDagger = true
-//   }
-//   // adding to the players score if they got the answer correct
-//   // but also checking to see if it has answers in the first place before adding to the score if the player got the answer correct
-//   if (game[game.currentLvl].options[parseInt(game.selectedAnswer)] && game[game.currentLvl].options[parseInt(game.selectedAnswer)].answer === true) {
-//     game.score++
-//   }
-//   console.log(game.score) 
-//   // let the program know that when the next button is clicked it should check which scene we are in
-//   game.currentLvl = game.position[index]
-//   // set the style of the background image to the image labeled backdrop inside the openScene component inside the game object
-//   contentBox.setAttribute("style", `background-image: ${game[game.currentLvl].backdrop}`)
-// // we gave it the text from the game assigned to the title category in the game object and tell it to load the title for the current scene
-//   title.innerText = game[game.currentLvl].title
-//   //we took the title from the game/openScene and put it into the DOM so that it will show up on the screen 
-//   contentBox.appendChild(title)
-//   // when entering the scene it will load the game text/content for that scene
-//   gameContent.innerText = game[game.currentLvl].content
-//   contentBox.appendChild(gameContent)
-
-//   function choiceCheck(e) {
-//     // first we need to look at the action id, to see if it has one
-//     // then we will check, if it exists, if it is empty
-//     if (e.target.id) {
-//       game.selectedItem = e.target.id
-//       // the ext as the description for the items/answers
-//       // will read the data attribute and turn the string into an integer 
-//       document.getElementsByClassName('gameText')[0].textContent = 
-//         game[game.currentLvl].options[parseInt(e.target.getAttribute('data-index'))].description;
-//       //storing the index of the players choice 
-//     } else {
-//       game.selectedAnswer = e.target.getAttribute('data-index')
-//     }
-//   }
-//   // clear the previous buttons
-//   optionBox.innerHTML = ""
-//   // when entering the scene if it has options it will generate option choice buttons
-//   if (game[game.currentLvl].options) {
-//     for (let i = 0; i < game[game.currentLvl].options.length; i++) {
-//       let choice = document.createElement('button')
-//       choice.innerText = game[game.currentLvl].options[i].action
-//       // checking to see if the object has an id
-//       if (game[game.currentLvl].options[i].hasOwnProperty('id')) {
-//         choice.setAttribute("id", `${game[game.currentLvl].options[i].id}`)
-//       }
-//       //create additional data in the html which will let us store the index of the item they clicked on 
-//       choice.setAttribute("data-index", i) 
-//       // if there is an id it will set one, if there is not it will not, which will let us determine if there is a inventory check or not
-//       optionBox.appendChild(choice)
-//       choice.addEventListener('click', choiceCheck) 
-//     } 
-//   }
-//   // it moves forward the place we are in in the scene array
-//   if (game.position.length != index + 1) {
-//     index ++
-//   }
-// })
-
-// let backButton = document.querySelector('.back')
-// backButton.addEventListener('click', () => {
-//   game.currentLvl = game.position[index]
-//   contentBox.style.backgroundImage = game[game.currentLvl].backdrop
-//   title.innerText = game[game.currentLvl].title
-//   contentBox.appendChild(title)
-//   gameContent.innerText = game[game.currentLvl].content
-//   contentBox.appendChild(gameContent)
-//   // it moves back the place we are in in the scene array
-//   if (index != 0) {
-//   index --
-//   }
-// })
-
-
+// here we are telling the game to display the score and reset itself at the end of the countdown
+function endGame() {
+  score.innerHTML = "score: " + game.score + "/6"
+  setTimeout(() => {
+    location.reload()
+  }, 15000);
+}
